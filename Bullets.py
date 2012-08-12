c209df46bc32e8a02063976abb8a1cacd8e3ffab
David Dossett <david@ddossett.com>
2012-08-11 23:33:23 -0500

    -Added some slightly broken ball-ball collision using the group's update method and sprite functions. -changed the way the relative position is calculated to just a function rather than class. -solved a few bugs in the screen edge collision detection.

--- Bullets.py
+++ Bullets.py
@@ -4,30 +4,92 @@ import math
 
 PixelSize = ntuple('PixelSize', 'w h')
 
+class Bullets(pygame.sprite.Group):
+    def __init__(self,sprite=()):
+        pygame.sprite.Group.__init__(self,sprite)
+    def update(self,milliseconds):
+        self.CheckSpriteCollisions()
+        pygame.sprite.Group.update(self,milliseconds)
+    def CheckSpriteCollisions(self):
+        for sprite1 in self.sprites():
+            for sprite2 in self.sprites():
+                if sprite1!=sprite2:
+                    if pygame.sprite.collide_circle(sprite1, sprite2):
+                        sprite1.Collision(sprite2)
+                    else: continue
+                else: continue
+
 class Bullet(pygame.sprite.Sprite):
     screen_size = PixelSize(640,480)
-    speed = 50.
+    speed = 0.5
     size = PixelSize(10,10)
-    def __init__(self, surface, mouse_pos ):
+    def __init__(self, surface, origin_pos ,mouse_pos ):
         pygame.sprite.Sprite.__init__(self)
         self.image = surface
         self.rect = self.image.get_rect()
+        self.rect.center = origin_pos
+        self.pos = [float(origin_pos[0]), float(origin_pos[1])]
+        self.radius = Bullet.size.w/2
         scale = 1/math.sqrt( math.pow(mouse_pos.x,2) + math.pow(mouse_pos.y,2) )
-        dx = scale * mouse_pos.x * Bullet.speed
-        dy = -scale * mouse_pos.y * Bullet.speed
-        self.velocity = [dx, dy]
+        vx = scale * mouse_pos.x * Bullet.speed
+        vy = -scale * mouse_pos.y * Bullet.speed
+        self.velocity = [vx, vy]
 
-    def update(self, seconds):
+    def update(self, milliseconds):
         if self.rect.left < 0: 
-            self.rect.left = 0
+            self.pos[0] = Bullet.size.w/2 
             self.velocity[0] = -self.velocity[0]
         elif self.rect.right > Bullet.screen_size.w:
-            self.rect.right = Bullet.screen_size.w
+            self.pos[0] = Bullet.screen_size.w - Bullet.size.w/2
             self.velocity[0] = -self.velocity[0]
         if self.rect.top < 0:
-            self.rect.top = 0
+            self.pos[1] = Bullet.size.h/2
             self.velocity[1] = -self.velocity[1]
         elif self.rect.bottom > Bullet.screen_size.h:
-            self.rect.bottom = Bullet.screen_size.h
+            self.pos[1] = Bullet.screen_size.h - Bullet.size.h/2
             self.velocity[1] = -self.velocity[1]
-        self.rect = self.rect.move(self.velocity[0]*seconds, self.velocity[1]*seconds)
+        dx = self.velocity[0]*milliseconds
+        dy = self.velocity[1]*milliseconds
+        self.pos[0] = self.pos[0]+dx
+        self.pos[1] = self.pos[1]+dy
+        self.rect.center = tuple(self.pos)
+
+    def Collision(self,other):
+        speed = math.sqrt(math.pow(self.velocity[0],2)+math.pow(self.velocity[1],2))
+        dx = other.pos[0]-self.pos[0]
+        dy = other.pos[1]-self.pos[1]
+        if dx > 0:
+            if dy > 0:
+                angle = math.degrees(math.atan(dy/dx))
+                vx = -speed*math.cos(math.radians(angle))
+                vy = -speed*math.sin(math.radians(angle))
+            elif dy < 0:
+                angle = math.degrees(math.atan(dy/dx))
+                vx = -speed*math.cos(math.radians(angle))
+                vy = -speed*math.sin(math.radians(angle))
+        elif dx < 0:
+            if dy > 0:
+                angle = 180 + math.degrees(math.atan(dy/dx))
+                vx = -speed*math.cos(math.radians(angle))
+                vy = -speed*math.sin(math.radians(angle))
+            elif dy < 0:
+                angle = -180 + math.degrees(math.atan(dy/dx))
+                vx = -speed*math.cos(math.radians(angle))
+                vy = -speed*math.sin(math.radians(angle))
+        elif dx == 0:
+            if dy > 0:
+                angle = -90
+            else:
+                angle = 90
+            vx = speed*math.cos(math.radians(angle))
+            vy = speed*math.sin(math.radians(angle))
+        elif dy == 0:
+            if dx < 0:
+                angle = 0
+            else:
+                angle = 180
+            vx = speed*math.cos(math.radians(angle))
+            vy = speed*math.sin(math.radians(angle))
+        self.velocity[0] = vx
+        self.velocity[1] = vy
+
--- ballshooter.py
+++ ballshooter.py
@@ -2,7 +2,7 @@ import os, sys, pygame
 import time
 import math
 from collections import namedtuple as ntuple
-from Bullets import Bullet
+from Bullets import Bullets,Bullet
 
 #pygame.mixer.pre_init(44100, -16, 2, 2048) # setup mixer to avoid sound lag
 pygame.init()
@@ -15,7 +15,7 @@ RGB = ntuple('RGB', 'r g b')
 # initialize some global varables with magic numbers
 SCREENSIZE = PixelSize(640, 480)
 HEROSIZE = PixelSize(50,50)
-BULLETSIZE = PixelSize(10,10)
+BULLETSIZE = PixelSize(20,20)
 CURSORRADIUS = BULLETSIZE.h/2
 HEROORIGIN = PixelPos(SCREENSIZE.w/2,SCREENSIZE.h)
 BULLETORIGIN = PixelPos(HEROORIGIN.x,HEROORIGIN.y-BULLETSIZE.h)
@@ -24,13 +24,14 @@ LINECOLOUR = RGB(20, 200, 20)
 CURSORCOLOUR = RGB(255, 0, 0)
 MAXFPS = 100
 FONTSIZE = 16
-BULLETSPEED = 250.
+BULLETSPEED = 0.25
 STATS = False
 
 ball_angle_prev = 0.
 ball_angle = 0.
 mouse_pos = PixelPos(0,0)
-bullets = pygame.sprite.Group()
+#bullets = pygame.sprite.Group()
+bullets = Bullets()
 Bullet.screen_size = SCREENSIZE
 Bullet.speed = BULLETSPEED
 Bullet.size = BULLETSIZE
@@ -52,15 +53,13 @@ clock = pygame.time.Clock()
 RUNNING = True
 # game loop
 
-class HeroPos(object):
-    def __init__(self,origin):
-        self.origin = origin
-    def CorrectedPos(self,pos):
-        self.pos = pos
-        relx = self.pos.x - self.origin.x
-        rely = self.origin.y - self.pos.y
-        self.relpos = PixelPos(relx,rely)
-        return self.relpos
+def CorrectPos(pos,origin):
+        relx = pos.x - origin.x
+        rely = origin.y - pos.y
+        relpos = PixelPos(relx,rely)
+        return relpos
+
+corr_pos = CorrectPos(mouse_pos,HEROORIGIN)
 
 while RUNNING:
     # limit the frame rate and calculate the real one
@@ -82,28 +81,26 @@ while RUNNING:
             else: continue
         elif evtType == pygame.MOUSEMOTION:
             mouse_pos = PixelPos(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
-            hp = HeroPos(HEROORIGIN)
-            corr_pos = hp.CorrectedPos(mouse_pos)
+            corr_pos = CorrectPos(mouse_pos,HEROORIGIN)
             ball_angle_rad = math.atan2(float(corr_pos.y),float(corr_pos.x))
             ball_angle = math.degrees(ball_angle_rad)
         elif evtType == pygame.MOUSEBUTTONDOWN:
             mouse_pos = PixelPos(pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1])
-            hp = HeroPos(HEROORIGIN)
-            corr_pos = hp.CorrectedPos(mouse_pos)
-            b = Bullet(ballBullet_scaled,corr_pos)
-            b.rect.center = BULLETORIGIN
+            bulletmouse_pos = CorrectPos(mouse_pos,BULLETORIGIN)
+            b = Bullet(ballBullet_scaled,BULLETORIGIN,bulletmouse_pos)
             bullets.add(b)
 
     ballHero = pygame.transform.rotate( ballHero_scaled, ball_angle )
     ballHero_rect = ballHero.get_rect()
     ballHero_rect.center = HEROORIGIN
+    bullets.update(milliseconds)
+
     pygame.draw.circle(screen, CURSORCOLOUR, mouse_pos, CURSORRADIUS, 2) # red circle
     screen.blit(ballHero, ballHero_rect)
-    bullets.update(seconds)
     for b in bullets.sprites(): screen.blit(b.image,b.rect)
     if STATS:
         myFont = pygame.font.SysFont("None", FONTSIZE)
-        screen.blit(myFont.render("FPS: %.2f" %fps, 0, LINECOLOUR), (10,10))
+        screen.blit(myFont.render("FPS: %.2f    Mouse Pos: (%i,%i)" %(fps, corr_pos.x, corr_pos.y), 0, LINECOLOUR), (10,10))
     pygame.display.flip()
 
 sys.exit()
